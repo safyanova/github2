96309ba00186ca8382d61a977de4d3f1ec77dad8
safyanova <vas18saf@mail.ru>
2026-06-23 15:24:29 +0300

    Update date

--- tests/test_login.py
+++ tests/test_login.py
@@ -9,16 +9,12 @@ class TestPages:
     @allure.title("Login")
     @allure.link(url="fffhhg", name="Documention")
     @pytest.mark.smoke
-    def test_open_login_page(self):
+    def test_open_login_page(self, driver):
         with allure.step("Step1"):
             self.driver.get("https://demoqa.com/login")
             assert self.driver.current_url == "https://demoqa.com/login", "Ошибка ULR страницы входа"
 
     @pytest.mark.regression
-    def test_open_books_page(self):
+    def test_open_books_page(self, driver):
         self.driver.get("https://demoqa.com/books")
         assert self.driver.current_url == "https://demoqa.com/books", "Ошибка ULR страницы с книгами"
-
-    @pytest.mark.profile    def test_open_profile_page(self):
-        self.driver.get("https://google.com")
-        assert self.driver.current_url == "https://google.com", "Ошибка ULR страницы профиля"
